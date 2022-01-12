d1c43b354a0e3204f33f114891fa3acaa61a633e
lkt226 <victorhugods.pereira@gmail.com>
2022-01-12 12:16:33 -0300

    go to site

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="body">
-      <component v-for="(value, index) in size" :key="index"
-          :is="'c'+index" class="grid-area" :style="`--i: ${index};`"/>
+      <div v-for="(value, index) in size" :key="index" :style="`--i: ${index};`">
+        <button class="c-info" @click="goToCode(index)">Ver o codigo</button>
+        <component :is="'c'+index" class="grid-area"/>
+      </div>
   </div>
 </template>
 
@@ -32,6 +34,11 @@ export default {
     }
 
     this.size = counter;
+  },
+  methods:{
+    goToCode(id){
+      window.open(`https://github.com/Lkt226/components/blob/master/projects/c${id}.vue`, '_blank')
+    }
   }
 }
 </script>
@@ -45,11 +52,21 @@ export default {
 
     .grid-area{
       @apply bg-light-600 p-6 shadow-lg relative;
+    }
+
+    .c-info{
+      @apply relative bg-gray-200
+              p-3 mb-2 rounded-sm
+              font-bold font-mono text-center text-gray-500; 
+
+      &:hover{
+        @apply text-gray-200 bg-gray-500;
+      }
+
       &::before{
         counter-reset: i var(--i);
-        content: counter(i);
-
-        @apply absolute left-2 top-0;
+        content: 'id:c'counter(i);
+        @apply pr-4;
       }
     }
   }
